5fd377c4271be04be59316bbc7bdd141039008d8
David Lozano <david.lozano.hurtado@gmail.com>
2018-03-25 14:09:05 +0200

    Commit inicial. Empezando con spring boot 2, gradle, mongo db y git :)

---
+++ demo-gradle/src/main/java/org/demogradle/com/DemoGradleApplication.java
@@ -0,0 +1,12 @@
+package org.demogradle.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradleApplication.class, args);
+	}
+}
