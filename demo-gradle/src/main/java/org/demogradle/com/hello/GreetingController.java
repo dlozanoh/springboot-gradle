22b3f61132da402ac437cd75aa8aa4ba85198283
David Lozano <david.lozano.hurtado@gmail.com>
2018-03-29 12:25:10 +0200

    controlador

---
+++ demo-gradle/src/main/java/org/demogradle/com/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package org.demogradle.com.hello;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+	private static Logger log = LogManager.getLogger(GreetingController.class);
+    
+	@GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		log.info("greeting () - " + name);
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
